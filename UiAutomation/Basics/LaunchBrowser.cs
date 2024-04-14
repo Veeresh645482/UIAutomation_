3527f71e611dc605767993775167be45ab56630f
Veeresh <imveeresh6454@gmail.com>
2024-04-14 08:42:03 +0530

    Adding UiAutomation Solution

---
+++ UiAutomation/Basics/LaunchBrowser.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace UiAutomation.Basics
+{
+    public class LaunchBrowser
+    {
+        private IWebDriver _driver;
+
+        [SetUp]
+        public void Intialization()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Url = "https://www.google.com/";
+        }
+
+        [Test]
+        public void Test()
+        {
+            Console.WriteLine(_driver.Title);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+        }
+        
+
+
+    }
+}
