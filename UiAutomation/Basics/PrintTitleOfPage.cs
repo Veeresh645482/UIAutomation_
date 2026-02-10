e44dbbbb1bdea4564e4ab10333507a66a9935457
Veeresh645482 <166889020+Veeresh645482@users.noreply.github.com>
2026-02-10 22:31:26 +0530

    Comment out code in PrintTitleOfPage.cs

--- UiAutomation/Basics/PrintTitleOfPage.cs
+++ UiAutomation/Basics/PrintTitleOfPage.cs
@@ -1,43 +1,43 @@
-﻿using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WebDriverManager.DriverConfigs.Impl;
+// ﻿using OpenQA.Selenium.Chrome;
+// using OpenQA.Selenium;
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Text;
+// using System.Threading.Tasks;
+// using WebDriverManager.DriverConfigs.Impl;
 
-namespace UiAutomation.Basics
-{
-    public class PrintTitleOfPage
-    {
-        private IWebDriver _driver;
+// namespace UiAutomation.Basics
+// {
+//     public class PrintTitleOfPage
+//     {
+//         private IWebDriver _driver;
 
-        [SetUp]
-        public void Intialization()
-        {
-            ChromeOptions options = new ChromeOptions();
-options.AddArgument("--headless=new");
-options.AddArgument("--no-sandbox");
-options.AddArgument("--disable-dev-shm-usage");
-options.AddArgument("--disable-gpu");
-options.AddArgument("--window-size=1920,1080");
+//         [SetUp]
+//         public void Intialization()
+//         {
+//             ChromeOptions options = new ChromeOptions();
+// options.AddArgument("--headless=new");
+// options.AddArgument("--no-sandbox");
+// options.AddArgument("--disable-dev-shm-usage");
+// options.AddArgument("--disable-gpu");
+// options.AddArgument("--window-size=1920,1080");
 
-IWebDriver driver = new ChromeDriver(options);
+// IWebDriver driver = new ChromeDriver(options);
             
-            _driver.Url = "https://www.google.com/";
-        }
+//             _driver.Url = "https://www.google.com/";
+//         }
 
-        [Test]
-        public void Test()
-        {
-            Console.WriteLine("Title of page is " + _driver.Title);
-        }
+//         [Test]
+//         public void Test()
+//         {
+//             Console.WriteLine("Title of page is " + _driver.Title);
+//         }
 
-        [TearDown]
-        public void TearDown()
-        {
-            _driver?.Close();
-        }
-    }
-}
+//         [TearDown]
+//         public void TearDown()
+//         {
+//             _driver?.Close();
+//         }
+//     }
+// }
